d3ef7178d7247ecdb79d3cde481c69c58384c199
Katalina Tovt <katalinatovt@106.lan0.mk.uz.ua>
2019-11-01 22:36:03 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+    <title>telefon</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="date">
+<h1>Google представила Pixel 4 и Pixel 4 XL</h1>
+
+    <div class="shosy pucka">15 Октября, 2019, 18:21</div>
+    <div class="shosy pucka"> Михаил Сапитон</div>
+    <div class="pucka"> 685  </div>
+
+<p>Компания Google представила новое поколение смартфонов Pixel. Большинство спецификаций, как и дизайн устройства,
+    утекли в сеть задолго до официального анонса.</p>
+
+<p>Смартфон вышел в двух размерах: Pixel 4 и Pixel 4 XL.</p>
+<a href="https://www.apple.com" target="_blank">
+<img src="https://ain.ua/wp-content/uploads/2019/10/leaked_pixel_sizing.5-1024x538.jpg?x91963" alt="telefon">
+
+</a>
+<p>Спецификации:</p>
+<ul type="square">
+    <li>Дисплей на 5,7 (Full HD) или 6,3 дюйма (Quad HD).</li>
+    <li>Частота обновления экрана — 90 Гц.</li>
+    <li>Сканер отпечатков пальцев под экраном.</li>
+    <li>Процессор Qualcomm Snapdragon 855.</li>
+    <li>6 ГБ ОЗУ и 64 ГБ постоянной памяти.</li>
+    <li>Батарея на 2800 или 3700 мАч.</li>
+    <li>Две камеры: стандартная и телескопическая.</li>
+</ul>
+<hr>
+<p>Цены:</p>
+<ul type="circle">
+    <li>Pixel 4 — от $799.</li>
+    <li>Pixel 4 XL —от $899.</li>
+</ul>
+
+<p> <a href="https://www.google.com">google</a></p>
+</div>
+</body>
+
+
+
+
+
+
+
+</html>
